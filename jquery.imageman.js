ef934a3f07af358bce054733dde62bfede3fb253
Nagy Péter <nagy.pite@gmail.com>
2012-03-30 14:47:54 +0200

    clickable thumbnails, basic styles

--- jquery.imageman.js
+++ jquery.imageman.js
@@ -8,11 +8,10 @@
             return null;
           }
         , dialog: {
-            minHeight: 300
-          , minWidth: 300
-          , maxHeight: 500
-          , maxWidth: 700
+            height: 500
+          , width: 700
           , modal: true
+          , title: 'Image manager'
         }
         , templateBody: '<div class="imageman-body">'
                         + '<div class="imageman-viewer loading">'
@@ -25,43 +24,62 @@
                         +   '<div class="imageman-viewer-loader"></div>'
                         + '</div>'
                         + '<div class="imageman-browser">'
-                        +   '<a class="imageman-browser-button prev"></div>'
-                        +   '<a class="imageman-browser-button next"></div>'
-                        +   '<div class="imageman-thumbs"></div>'
+                        +   '<a class="imageman-browser-button left"></a>'
+                        +   '<a class="imageman-browser-button right"></a>'
+                        +   '<div class="imageman-thumbs imageman-clearfix"></div>'
                         + '</div>'
                         + '</div>'
         , templateThumb: '<div class="imageman-thumb"><img></div>'
         , baseSrc: ''
+        , source: null
       }
     , dialog: null
-    , opts: null
+    , opts: {}
   }
 
   $.fn.imageMan = function(opts) {
-    $.extend(true, $.imageMan.opts, opts, $.imageMan.defaults);
+    $.extend(true, $.imageMan.opts, $.imageMan.defaults, opts);
 
-    $(this).each( function() {
+    $(this).click( function() {
       var trigger = $(this);
-      $.getJSON($.imageMan.opts.source, function(data) {
+      $.getJSON($.imageMan.opts.source || trigger.attr('href'), function(data) {
         var images = $.imageMan.opts.extractor.apply(trigger, [data])
           , body = $($.imageMan.opts.templateBody);
+
+        if (!images) {
+          console.log('no image data', data);
+          return;
+        }
 
         $.each(images, function() {
           var image = this
             , thumb = $($.imageMan.opts.templateThumb);
 
           thumb.find('img')
-              .attr('src', $.imageMan.opts.baseSrc + image.src)
+              .attr('src', $.imageMan.opts.baseSrc + image.name)
               .attr('title', image.title || image.src);
 
-          thumb.data('src', image.src)
-              .appendTo(body.find('.imageman-thumbs'));
+          thumb.data('src', image.name);
+          body.find('.imageman-thumbs').append(thumb);
         });
 
         $.imageMan.opts.dialog.create = function(e, ui) {
+          var dialog = $(e.target);
+
+          dialog.delegate('.imageman-thumb', 'click', function() {
+            var viewer = dialog.find('.imageman-viewer')
+              , viewImg = dialog.find('.imageman-viewer-image img')
+              , thumbImg = $(this).find('img');
+
+            viewer.addClass('loading');
+            viewImg.load(function(){viewer.removeClass('loading')});
+            viewImg.attr('src', thumbImg.attr('src'));
+          });
         }
         $.imageMan.dialog = body.dialog($.imageMan.opts.dialog);
       });
+
+      return false;
     });
   }
 })(jQuery);
