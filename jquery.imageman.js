e69a08b3285bd8b4b8e81de096f58e78b4c984f0
nagy.pite <nagy.pite@gmail.com>
2012-04-01 19:47:27 +0200

    added option for loading instantly, select option

--- jquery.imageman.js
+++ jquery.imageman.js
@@ -37,6 +37,7 @@
         , baseSrc: ''
         , source: null
         , delete: null
+        , select: null
         , upload: typeof(qq) !== 'undefined'
         , uploader: {
             action: ''
@@ -86,118 +87,127 @@
         thumbs.animate({left: Math.min(Math.max(leftMin, leftPos), 0)});
       }
     , opts: {}
+    , open: function() {
+        var trigger = $(this);
+        $.getJSON($.imageMan.opts.source, function(data) {
+          var images = $.imageMan.opts.extractor(data)
+            , body = $($.imageMan.opts.templateBody);
+
+          if (!images) {
+            console.log('no image data', data);
+            return;
+          }
+
+          if (!$.imageMan.opts.delete) {
+            body.find('.imageman-viewer-control.delete').remove();
+          }
+
+          $.each(images, function() {
+            body.find('.imageman-thumbs').append($.imageMan.createThumb.apply(this));
+          });
+
+          if ($.imageMan.opts.upload) {
+            if (typeof(qq) != 'undefined' && qq.FileUploader) {
+              body.find('.imageman-thumbs').append($.imageMan.opts.templateUploadThumb);
+            } else {
+              alert('qq.FileUploader is not loaded');
+            }
+          }
+
+          $.imageMan.opts.dialog.create = function(e, ui) {
+            var dialog = $(e.target);
+
+            dialog.delegate('.imageman-thumb', 'click', function() {
+              var viewer = dialog.find('.imageman-viewer')
+                , thumb = $(this);
+
+              thumb.siblings('.selected').removeClass('selected');
+              thumb.addClass('selected');
+              $.imageMan.centerThumb.apply(thumb);
+
+              if (thumb.hasClass('imageman-thumb-upload')) {
+                var upload = viewer.find('.imageman-viewer-upload');
+
+                viewer.addClass('uploading');
+                if (!upload.hasClass('loaded')) {
+                  var uploaderOpts = $.extend({}, $.imageMan.opts.uploader);
+                  uploaderOpts.element = upload[0];
+                  uploaderOpts.onComplete = function(id, filename, json) {
+                    if (json.success) {
+                      thumb.before($.imageMan.createThumb.apply(json));
+                      $.imageMan.resizeThumbs();
+                      $.imageMan.centerThumb.apply(thumb);
+
+                    } else if (json.error) {
+                      alert('Upload failed: ' + json.error);
+
+                    } else {
+                      alert('Upload failed');
+                    }
+                  };
+                  $.imageMan.uploader = new qq.FileUploader(uploaderOpts);
+                  upload.addClass('loaded');
+                }
+
+              } else {
+                var viewImg = dialog.find('.imageman-viewer-image img')
+                  , thumbImg = thumb.find('img');
+
+                // TODO fade in&out
+                viewer.addClass('loading');
+                viewer.removeClass('uploading');
+                viewImg.load(function(){viewer.removeClass('loading')});
+                viewImg.attr('src', thumbImg.attr('src'));
+                viewImg.attr('title', thumbImg.attr('title'));
+                viewer.find('.imageman-viewer-title').text(thumbImg.attr('title'));
+                viewer.data('image', thumb.data('image'));
+                viewer.data('thumb', thumb);
+              }
+            });
+            dialog.delegate('.imageman-viewer-control.select', 'click', function() {
+              var viewer = $(this).parents('.imageman-viewer');
+              if ($.imageMan.opts.select) {
+                $.imageMan.opts.select.apply(trigger, [viewer.data('image')]);
+              }
+              trigger.trigger('select.imageman', viewer.data('image'));
+              $.imageMan.dialog.dialog('close');
+            });
+            dialog.delegate('.imageman-viewer-control.delete', 'click', function() {
+              var viewer = $(this).parents('.imageman-viewer')
+                , thumb = viewer.data('thumb')
+                , image = viewer.data('image');
+
+              if ($.imageMan.opts.delete) {
+                $.imageMan.opts.delete.apply(image, [thumb, $.imageMan.deleteCallback]);
+              }
+            });
+            dialog.delegate('.imageman-browser-button', 'click', function() {
+              var selected = dialog.find('.imageman-thumb.selected');
+              if ($(this).hasClass('left'))
+                selected.prev().click()
+              else
+                selected.next().click()
+            });
+            $.imageMan.resizeThumbs(dialog);
+            dialog.find('.imageman-thumb').first().click();
+          }
+          $.imageMan.dialog = body.dialog($.imageMan.opts.dialog);
+        });
+
+        return false;
+      }
   }
 
   $.fn.imageMan = function(opts) {
     $.extend(true, $.imageMan.opts, $.imageMan.defaults, opts);
 
-    $(this).click( function() {
-      var trigger = $(this);
-      $.getJSON($.imageMan.opts.source || trigger.attr('href'), function(data) {
-        var images = $.imageMan.opts.extractor.apply(trigger, [data])
-          , body = $($.imageMan.opts.templateBody);
+    if ($(this).length) {
+      $(this).click( $.imageMan.open );
+      return this;
 
-        if (!images) {
-          console.log('no image data', data);
-          return;
-        }
-
-        if (!$.imageMan.opts.delete) {
-          body.find('.imageman-viewer-control.delete').remove();
-        }
-
-        $.each(images, function() {
-          body.find('.imageman-thumbs').append($.imageMan.createThumb.apply(this));
-        });
-
-        if ($.imageMan.opts.upload) {
-          if (typeof(qq) != 'undefined' && qq.FileUploader) {
-            body.find('.imageman-thumbs').append($.imageMan.opts.templateUploadThumb);
-          } else {
-            alert('qq.FileUploader is not loaded');
-          }
-        }
-
-        $.imageMan.opts.dialog.create = function(e, ui) {
-          var dialog = $(e.target);
-
-          dialog.delegate('.imageman-thumb', 'click', function() {
-            var viewer = dialog.find('.imageman-viewer')
-              , thumb = $(this);
-
-            thumb.siblings('.selected').removeClass('selected');
-            thumb.addClass('selected');
-            $.imageMan.centerThumb.apply(thumb);
-
-            if (thumb.hasClass('imageman-thumb-upload')) {
-              var upload = viewer.find('.imageman-viewer-upload');
-
-              viewer.addClass('uploading');
-              if (!upload.hasClass('loaded')) {
-                var uploaderOpts = $.extend({}, $.imageMan.opts.uploader);
-                uploaderOpts.element = upload[0];
-                uploaderOpts.onComplete = function(id, filename, json) {
-                  if (json.success) {
-                    thumb.before($.imageMan.createThumb.apply(json));
-                    $.imageMan.resizeThumbs();
-                    $.imageMan.centerThumb.apply(thumb);
-
-                  } else if (json.error) {
-                    alert('Upload failed: ' + json.error);
-
-                  } else {
-                    alert('Upload failed');
-                  }
-                };
-                $.imageMan.uploader = new qq.FileUploader(uploaderOpts);
-                upload.addClass('loaded');
-              }
-
-            } else {
-              var viewImg = dialog.find('.imageman-viewer-image img')
-                , thumbImg = thumb.find('img');
-
-              // TODO fade in&out
-              viewer.addClass('loading');
-              viewer.removeClass('uploading');
-              viewImg.load(function(){viewer.removeClass('loading')});
-              viewImg.attr('src', thumbImg.attr('src'));
-              viewImg.attr('title', thumbImg.attr('title'));
-              viewer.find('.imageman-viewer-title').text(thumbImg.attr('title'));
-              viewer.data('image', thumb.data('image'));
-              viewer.data('thumb', thumb);
-            }
-          });
-          dialog.delegate('.imageman-viewer-control.select', 'click', function() {
-            var viewer = $(this).parents('.imageman-viewer');
-            trigger.trigger('select.imageman', viewer.data('image'));
-            $.imageMan.dialog.dialog('close');
-          });
-          dialog.delegate('.imageman-viewer-control.delete', 'click', function() {
-            var viewer = $(this).parents('.imageman-viewer')
-              , thumb = viewer.data('thumb')
-              , image = viewer.data('image');
-
-            if ($.imageMan.opts.delete) {
-              $.imageMan.opts.delete.apply(image, [thumb, $.imageMan.deleteCallback]);
-            }
-          });
-          dialog.delegate('.imageman-browser-button', 'click', function() {
-            var selected = dialog.find('.imageman-thumb.selected');
-            if ($(this).hasClass('left'))
-              selected.prev().click()
-            else
-              selected.next().click()
-          });
-          $.imageMan.resizeThumbs(dialog);
-          dialog.find('.imageman-thumb').first().click();
-        }
-        $.imageMan.dialog = body.dialog($.imageMan.opts.dialog);
-      });
-
-      return false;
-    });
-
-    return this;
+    } else {
+      $.imageMan.open.apply(document);
+      return $.imageMan.dialog
+    }
   }
 })(jQuery);
